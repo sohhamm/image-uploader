adf602d030e084425d2cf12939f0c4c0a8fb8b93
Soham Sarkar <soham.ss1@gmail.com>
2021-10-19 21:56:56 +0530

    improved UI

--- client/src/components/ImageArea/ImageArea.jsx
+++ client/src/components/ImageArea/ImageArea.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
 import classes from './ImageArea.module.css'
 
 export default function ImageArea({imageData}) {
-  console.log(imageData)
+  const handleCopyLink = async () => {
+    await navigator.clipboard.writeText(imageData)
+  }
   return (
     <div className={classes.container}>
       <div className={classes.imageContainer}>
         <img src={imageData} alt="uploaded-image" />
       </div>
       <div className={classes.copyLink}>
-        <p>{imageData}</p>
-        <button>Copy Link</button>
+        <p className={classes.link}>{imageData}</p>
+        <button onClick={handleCopyLink}>Copy Link</button>
       </div>
     </div>
   )
